9137557867466cb60d6e627675cd363ef29ec327
MikeANM <miguel@newmerida.com>
2024-05-23 00:00:18 -0600

    instalacion del orm

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 //Importar express
 import express from 'express';
 import usuariosRoutes from './routes/usuariosRoutes.js';
+import db from './config/db.js';
 
 //Crear la app
 const app = express();
 
-//Routing
-/*
-Use es un método que se utiliza para especificar una función de middleware.
-*/
-app.use('/auth', usuariosRoutes);
+//Conectar a la base de datos
+try{
+ await db.authenticate();
+ console.log('Conexion correcta');
+}catch(error){
+    console.log(error);
+}
+
 
 //Habilitar Pug
 app.set('view engine', 'pug');
@@ -18,6 +22,12 @@ app.set('views', './views');
 //Carpeta Pública
 //Aqui  se almacenan los archivos estáticosvv
 app.use(express.static('public'));
+
+//Routing
+/*
+Use es un método que se utiliza para especificar una función de middleware.
+*/
+app.use('/auth', usuariosRoutes);
 
 //Definir un puerto y arrancar el servidor
 const port = 3000;
